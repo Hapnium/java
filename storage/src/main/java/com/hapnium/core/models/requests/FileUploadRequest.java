0a9eeda81adf76d850241922f9e78f199dd44b9e
Evaristus Adimonyemma <evaristusadimonyemma@gmail.com>
2025-05-30 01:17:03 +0100

    Fixing hasContent in FileUploadRequest

--- storage/src/main/java/com/hapnium/core/models/requests/FileUploadRequest.java
+++ storage/src/main/java/com/hapnium/core/models/requests/FileUploadRequest.java
@@ -61,6 +61,6 @@ public class FileUploadRequest {
      * @return true or false.
      */
     public boolean hasContent() {
-        return path.isEmpty() || bytes == null;
+        return !path.isEmpty() || get() != null;
     }
 }
